5b60d95d4144df15fb6b6093bc982b26c818be79
rakai <ra9g16@soton.ac.uk>
2017-07-15 10:57:22 +0100

    GhostTower Staging

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	private GameObject MainCam;
+	private GameObject SubCam;
+
+	void Start () {
+		MainCam = GameObject.Find("MainCamera");
+		SubCam = GameObject.Find("SubCamera");
+
+		SubCam.SetActive(false);
+	}
+
+	void Update () {
+		if(Input.GetKeyDown("space")){
+			if(MainCam.activeSelf){
+				MainCam.SetActive (false);
+				SubCam.SetActive (true);
+			}else{
+				MainCam.SetActive (true);
+				SubCam.SetActive (false);
+			}
+		}
+	}
+
+}
